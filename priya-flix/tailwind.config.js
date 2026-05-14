e138a5a4699471b34a2c165648d7a6b877c7b2c1
Nikunj Variya <75500096+itznik@users.noreply.github.com>
2026-05-14 12:13:46 +0530

    Create tailwind.config.js

---
+++ priya-flix/tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        netflix: '#e50914',
+        dark: '#141414',
+      },
+      backgroundImage: {
+        'hero-pattern': "linear-gradient(to top, rgba(0, 0, 0, 0.8) 0, rgba(0, 0, 0, 0) 60%, rgba(0, 0, 0, 0.8) 100%)",
+      }
+    },
+  },
+  plugins: [],
+};
